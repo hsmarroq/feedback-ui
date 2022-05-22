8e6d031d8276691c24e807e020bce10763a44bf3
Herson Marroquin <hersonmarroquin30@gmail.com>
2022-05-22 04:21:57 -0700

    moving functions to context

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,49 @@
+import { v4 as uuidv4 } from 'uuid';
+import { createContext, useState } from 'react';
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: 'This is feedback item one.',
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: 'This is feedback item two.',
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: 'This is feedback item three.',
+      rating: 7,
+    },
+  ]);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
